a007a565f3ed7c59d9287bdc8a7809969b577750
The GOAT from billionairs.com <you@example.com>
2025-06-25 13:54:15 +0200

    finished ex02

--- ex02/ft_print_reverse_alphabet.c
+++ ex02/ft_print_reverse_alphabet.c
@@ -1,10 +1,27 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vwalker <vwalker@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/25 13:41:11 by vwalker           #+#    #+#             */
+/*   Updated: 2025/06/25 13:53:40 by vwalker          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void my_putchar(char c)
+{
+  write(1, &c, 1);
+}
 
 void ft_print_reverse_alphabet(void) {
-    char* c = "z";
+    char c;
     
-    while (c >= 'a') {
-        printf("%s", c);
+    for (c = 'z'; c >= 'a';) {
+        my_putchar(c);
         c--;
     }
 }

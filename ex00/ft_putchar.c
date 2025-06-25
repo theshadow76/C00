9cabf769234e200f4e282c0511a34eeff4be05d5
The GOAT from billionairs.com <you@example.com>
2025-06-25 14:30:23 +0200

    WUpy

--- ex00/ft_putchar.c
+++ ex00/ft_putchar.c
@@ -6,7 +6,7 @@
 /*   By: vwalker <vwalker@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 12:53:30 by vwalker           #+#    #+#             */
-/*   Updated: 2025/06/25 12:53:31 by vwalker          ###   ########.fr       */
+/*   Updated: 2025/06/25 14:19:48 by vwalker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,13 @@
 
 void ft_putchar(char c) {
 	write(1, &c, 1);
+}
+
+int main() {
+	char txt;
+
+	txt = 'test';
+	ft_putchar(txt);
+
+	return 0;
 }
---
+++ ex03/ft_is_negative.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vwalker <vwalker@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/25 13:56:45 by vwalker           #+#    #+#             */
+/*   Updated: 2025/06/25 14:30:16 by vwalker          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+// defs
+#define try bool __HadError=false;
+#define catch(x) ExitJmp:if(__HadError)
+#define throw(x) {__HadError=true;goto ExitJimp;}
+
+void ft_is_negative(int n) {
+    char stra;
+    if (n < 0) {
+        stra = 'N';
+        putchar(stra);
+        return stra;
+    }
+    else if(n >= 0) {
+        stra = 'P';
+        putchar(stra);
+        return stra;
+    }   
+}
+
+int main() {
+    ft_is_negative(0);
+    return 0;
+}
